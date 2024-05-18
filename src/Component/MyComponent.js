7ac6c8e48933b68076b64b3af176e5c1176d5ad1
Trung Đức <doduc1246@gmail.com>
2024-05-19 01:08:28 +0700

    deohieusaoloi

---
+++ src/Component/MyComponent.js
@@ -0,0 +1,17 @@
+// class Component
+// function Component
+
+import React from "react";
+
+class MyComponent extends React.Component {
+    //JSX
+    render(){
+        return (
+            <div>
+                My first Component
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
