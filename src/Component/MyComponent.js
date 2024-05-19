09eac9b8cbd40bc3a39bacba3a60c458f80927f5
Trung Đức <doduc1246@gmail.com>
2024-05-20 01:59:07 +0700

    Video 20

--- src/Component/MyComponent.js
+++ src/Component/MyComponent.js
@@ -11,11 +11,31 @@ class MyComponent extends React.Component {
         age : 26
     }
     
+    handleClick = (event) =>{
+        console.log(">> Click me my button");
+        
+        //merge State => react class
+        this.setState({
+            name : 'Duc',
+            age : Math.floor((Math.random() * 100) +1)
+        })
+
+        // this.setState({
+        //     age : Math.floor((Math.random() * 100) +1)
+        // })
+    }
+
+    handleOnMoverOver(event){
+        // console.log(event.pageX)
+    }
+
     //JSX
     render(){
         return (
             <div>
-                My name is {this.state.name} and I'm from {this.state.address}
+                My name is {this.state.name} and I'm from {this.state.age}
+                <button onMouseOver={this.handleOnMoverOver}>Hover me</button>
+                <button onClick={(event) => {this.handleClick(event)}}>Click me</button>
             </div>
         );
     }
