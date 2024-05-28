b6d5a3d6ee38a18182674faa23addca4b917826f
Trung Đức <doduc1246@gmail.com>
2024-05-29 02:23:20 +0700

    deo hieu sao ko dung duoc boostrap

--- src/App.js
+++ src/App.js
@@ -5,38 +5,18 @@ import './App.scss';
 import MyComponent from './component/MyComponent';
 import React from 'react';
 
-class App extends React.Component {
 
-  render(){
-    return(
-      <div className='app-container'>
-      Hello World &amp; Trung Duc
-      <MyComponent></MyComponent>
-    </div> 
-    );
-  }
+
+
+
+const App = () => {
+
+  return (
+    <div className="App">
+      Hello World
+      <button class ="btn btn-primary">Test</button>
+    </div>
+  );
 }
-
-
-
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello world &amp; Trung Duc
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
