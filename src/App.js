c445d89f7c857d8c05c5b6342fefe99c17a2afca
tduc16 <doduc1246@gmail.com>
2024-05-31 13:12:08 +0700

    lai loi cmnr

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import Header from './component/Header/Header';
 import './App.scss';
+import { BrowserRouter as Router } from 'react-router-dom';  
+import {Link} from 'react-router-dom';
 const App = () => {
 
   return (
+    <Router>
     <div className="app-container">
       <Header />
-
+      <div>
+        test Link
+        <div>
+          <button>
+            <Link to ="/users">go to user page</Link>
+            
+            </button>
+          <button>
+            <Link to ="/admins">go to admin page</Link>
+            
+            </button>
+        </div>
+      </div>
     </div>
+    </Router>
   );
 }
 export default App;
