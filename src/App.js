8169a4d19d9d1c4737f87a4760d8f847319ff523
tduc16 <doduc1246@gmail.com>
2024-06-01 02:30:25 +0700

    Outlet React router dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import Header from './component/Header/Header';
 import './App.scss';
-import { Link } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
+
 
 const App = () => {
   return (
     <div className="app-container">
-      <Header />
-      <div>
-        test Link
-        <div>
-          <button>
-            <Link to="/users">go to user page</Link>
-          </button>
-          <button>
-            <Link to="/admins">go to admin page</Link>
-          </button>
-        </div>
+      <div className='header-container'>
+            <Header />
       </div>
+      <div className='main-container'>
+       <div className='sidenav-container'>
+
+      </div>
+      <div className='app-content'>
+        <Outlet />
+      </div>
+      </div>
+      
     </div>
   );
 };
